ed0b191fd07af5b8f983d26b8b336776c6b3d123
David Amos <somacdivad@gmail.com>
2017-11-26 21:58:45 -0600

    Documented invariants/dsi.py

--- grinpy/invariants/dsi.py
+++ grinpy/invariants/dsi.py
@@ -60,8 +60,9 @@ def sub_k_domination_number(G, k):
 
     References
     ----------
-    D. Amos, J. Asplund, and R. Davila, The sub-k-domination number of a graph
-    with applications to k-domination, *arXiv preprint arXiv:1611.02379*, (2016)
+    D. Amos, J. Asplund, B. Brimkov and R. Davila, The sub-k-domination number
+    of a graph with applications to k-domination, *arXiv preprint
+    arXiv:1611.02379*, (2016)
     """
     # TODO: add check that k >= 1 and throw error if not
     D = degree_sequence(G)
@@ -74,11 +75,83 @@ def sub_k_domination_number(G, k):
     return None
 
 def slater(G):
-    # TODO: Add documentation
+    """Return the Slater invariant for the graph.
+
+    The Slater invariant is a lower bound for the domination number of a graph
+    defined by:
+
+    .. math::
+        sl(G) = \min{t : t + \sum_{i=0}^t d_i \geq n}
+
+    where
+
+    .. math::
+        {d_1 \geq d_2 \geq \cdots \geq \d_n}
+
+    is the degree sequence of the graph ordered in non-increasing order.
+
+    Amos et al. rediscovered this invariant an generalized it into what is
+    now known as the sub-domination number.
+
+    Parameters
+    ----------
+    G : graph
+        A Networkx graph.
+
+    Returns
+    -------
+    slater : int
+        The Slater invariant for the graph.
+
+    See Also
+    --------
+    sub_k_domination_number
+
+    References
+    ----------
+    D. Amos, J. Asplund, B. Brimkov and R. Davila, The sub-k-domination number
+    of a graph with applications to k-domination, *arXiv preprint
+    arXiv:1611.02379*, (2016)
+
+    P.J. Slater, Locating dominating sets and locating-dominating set, *Graph
+    Theory, Combinatorics and Applications: Proceedings of the 7th Quadrennial
+    International Conference on the Theory and Applications of Graphs*,
+    2: 2073-1079 (1995)
+    """
     return sub_k_domination_number(G, 1)
 
 def sub_total_domination_number(G):
-    # TODO: Add documentation
+    """Return the sub-total domination number of the graph.
+
+    The sub-total domination number is defined as:
+
+    .. math::
+        sub_{t}(G) = \min{t : \sum_{i=0}^t d_i \geq n}
+
+    where
+
+    .. math::
+        {d_1 \geq d_2 \geq \cdots \geq \d_n}
+
+    is the degree sequence of the graph ordered in non-increasing order.
+
+    This invariant was defined and investigated by Randy Davila.
+
+    Parameters
+    ----------
+    G : graph
+        A Networkx graph.
+
+    Returns
+    -------
+    subTotalDominationNumber : int
+        The sub-total domination number of the graph.
+
+    References
+    ----------
+    R. Davila, A note on sub-total domination in graphs. *arXiv preprint
+    arXiv:1701.07811*, (2017)
+    """
     D = degree_sequence(G)
     D.sort(reverse = True)
     n = len(D)
@@ -89,7 +162,30 @@ def sub_total_domination_number(G):
     return None
 
 def annihilation_number(G):
-    # TODO: Add documentation
+    """Return the annihilation number of the graph.
+
+    The annihilation number is defined as:
+
+    .. math::
+        a(G) = \max{t : \sum_{i=0}^t d_i \leq n}
+
+    where
+
+    .. math::
+        {d_1 \leq d_2 \leq \cdots \leq \d_n}
+
+    is the degree sequence of the graph ordered in non-decreasing order.
+
+    Parameters
+    ----------
+    G : graph
+        A Networkx graph.
+
+    Returns
+    -------
+    annihilationNumber : int
+        The annihilation number of the graph.
+    """
     D = degree_sequence(G)
     D.sort() # sort in non-decreasing order
     m = number_of_edges(G)
