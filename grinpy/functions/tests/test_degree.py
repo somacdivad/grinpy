a74ee9e519def84d32ec7c3147dfb9030f13a7a7
David Amos <somacdivad@gmail.com>
2017-11-30 01:56:15 -0600

    Update unit tests for improved coverage

--- grinpy/functions/tests/test_degree.py
+++ grinpy/functions/tests/test_degree.py
@@ -12,25 +12,33 @@ class TestDegree():
         self.G = G
 
     def test_degree_sequence(self):
-        assert(gp.degree_sequence(self.G) == [3, 2, 2, 3, 1, 1])
+        D = gp.degree_sequence(self.G)
+        assert(D == [3, 2, 2, 3, 1, 1])
 
     def test_max_degree(self):
-        assert(gp.max_degree(self.G) == 3)
+        maxDegree = gp.max_degree(self.G)
+        assert(maxDegree == 3)
 
     def test_min_degree(self):
-        assert(gp.min_degree(self.G) == 1)
+        minDegree = gp.min_degree(self.G)
+        assert(minDegree == 1)
 
     def test_average_degree(self):
-        assert(gp.average_degree(self.G) == 2)
+        avgDegree = gp.average_degree(self.G)
+        assert(avgDegree == 2)
 
     def test_number_of_nodes_of_degree_k(self):
-        assert(gp.number_of_nodes_of_degree_k(self.G, 2) == 2)
+        numNodes = gp.number_of_nodes_of_degree_k(self.G, 2)
+        assert(numNodes == 2)
 
     def test_number_of_degree_one_nodes(self):
-        assert(gp.number_of_degree_one_nodes(self.G) == 2)
+        numNodes = gp.number_of_degree_one_nodes(self.G)
+        assert(numNodes == 2)
 
     def test_number_of_min_degree_nodes(self):
-        assert(gp.number_of_min_degree_nodes(self.G) == 2)
+        numNodes = gp.number_of_min_degree_nodes(self.G)
+        assert(numNodes == 2)
 
     def test_number_of_max_degree_nodes(self):
-        assert(gp.number_of_max_degree_nodes(self.G) == 2)
+        numNodes = gp.number_of_max_degree_nodes(self.G)
+        assert(numNodes == 2)
