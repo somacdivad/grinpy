d7f22a280d8bba1b468ace13848b4423262f9dbe
David Amos <somacdivad@gmail.com>
2017-11-30 01:03:10 -0600

    Update appveyor configuration and install dependencies

--- grinpy/__init__.py
+++ grinpy/__init__.py
@@ -11,8 +11,8 @@
 # check Python version
 # TODO: determine actual minimum version required. Here, the development version is used.
 import sys
-if sys.version_info[:2] < (3, 6):
-    m = "Python 3.6 or later is required for GrinPy (%d.%d detected)."
+if sys.version_info[:2] < (3, 4):
+    m = "Python 3.4 or later is required for GrinPy (%d.%d detected)."
     raise ImportError(m % sys.version_info[:2])
 del sys
 
