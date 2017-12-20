ab841a3684f94c8baa973b7257d3d7f7e7555bc7
David Amos <somacdivad@gmail.com>
2017-12-20 15:44:55 -0600

    package version 0.2.0a1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ sys.path.insert(1, wd)
 name = 'grinpy'
 author = 'David Amos, Randy Davila'
 email = 'amosd2@tamu.edu, davilar@uhd.edu'
-version = '0.1.0'
+version = '0.2.0a1'
 classifiers = [
     'Development Status :: 2 - Pre-Alpha',
     'Intended Audience :: Science/Research',
