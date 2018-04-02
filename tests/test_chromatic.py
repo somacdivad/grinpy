f23b79633dc8671351d6457deba7e748cbd75303
David Amos <somacdivad@gmail.com>
2018-04-02 00:20:28 -0500

    updated calls with  argument

--- tests/test_chromatic.py
+++ tests/test_chromatic.py
@@ -1,12 +1,14 @@
 import grinpy as gp
 
-class TestChromatic():
 
-    def test_chromatic_number_of_complete_graph_is_order(self):
-        for i in range(1, 11):
-            G = gp.complete_graph(i)
-            assert(gp.chromatic_number(G) == G.order())
+def test_chromatic_number_of_complete_graph_is_order():
+    for i in range(1, 11):
+        G = gp.complete_graph(i)
+        assert(gp.chromatic_number(G, method='ram-rama') == G.order())
+        assert(gp.chromatic_number(G, method='ilp') == G.order())
 
-    def test_chromatic_number_of_petersen_graph_is_3(self):
-        G = gp.petersen_graph()
-        assert(gp.chromatic_number(G) == 3)
+
+def test_chromatic_number_of_petersen_graph_is_3():
+    G = gp.petersen_graph()
+    assert(gp.chromatic_number(G, method='ram-rama') == 3)
+    assert(gp.chromatic_number(G, method='ilp') == 3)
