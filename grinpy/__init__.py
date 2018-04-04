62a746bd31089ead5dd4230eedffc47f2670e44b
David Amos <somacdivad@gmail.com>
2018-04-03 22:14:53 -0500

    added author, email and version info

--- grinpy/__init__.py
+++ grinpy/__init__.py
@@ -8,12 +8,18 @@
 # Authors: David Amos <somacdivad@gmail.com>
 #          Randy Davila <davilar@uhd.edu>
 
+"""Top-level package for GrinPy."""
+
 # check Python version
 import sys
 if sys.version_info[:2] < (3, 4):
     m = "Python 3.4 or later is required for GrinPy (%d.%d detected)."
     raise ImportError(m % sys.version_info[:2])
 del sys
+
+__author__ = """David Amos, Randy Davila"""
+__email__ = 'somacdivad@gmail.com, dvilar@uhd.edu'
+__version__ = '0.2.2'
 
 # import NetworkX dependency
 import networkx
