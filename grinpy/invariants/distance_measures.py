200555ee375d47c4bd6a5066008f0bd330fad333
David Amos <somacdivad@gmail.com>
2019-05-29 12:59:31 -0500

    Remove commented out code

--- grinpy/invariants/distance_measures.py
+++ grinpy/invariants/distance_measures.py
@@ -39,16 +39,8 @@ def triameter(G):
     https://arxiv.org/pdf/1804.01088.pdf
     """
     _distance = functools.partial(distance, G)
-    _combinations = itertools.combinations
     distance_sums = (
-        sum((_distance(*pair) for pair in _combinations(nodes, 2))) for nodes in _combinations(G.nodes(), 3)
+        sum((_distance(*pair) for pair in itertools.combinations(nodes, 2)))
+        for nodes in itertools.combinations(G.nodes(), 3)
     )
     return max(distance_sums)
-    # d = []
-    # for s in itertools.combinations(G.nodes(), 3):
-    #     x1 = distance(G, source=s[0], target=s[1])
-    #     x2 = distance(G, source=s[1], target=s[2])
-    #     x3 = distance(G, source=s[0], target=s[2])
-    #     d.append(x1 + x2 + x3)
-
-    # return max(d)
