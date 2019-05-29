816913b235ab93d2b41d6eac3ffb0b9d83589177
David Amos <somacdivad@gmail.com>
2019-05-29 15:14:31 -0500

    Add augmented Randic index

--- grinpy/invariants/topological_indices.py
+++ grinpy/invariants/topological_indices.py
@@ -20,7 +20,7 @@ import networkx as nx
 
 __all__ = [
     "randic_index",
-    # "augmented_randic_index",
+    "augmented_randic_index",
     # "harmonic_index",
     # "atom_bond_connectivity_index",
     # "sum_connectivity_index",
@@ -54,3 +54,32 @@ def randic_index(G):
     """
     _degree = functools.partial(nx.degree, G)
     return math.fsum(((_degree(e[0]) * _degree(e[1])) ** -0.5 for e in G.edges()))
+
+
+def augmented_randic_index(G):
+    r"""Returns the augmented Randic Index of the graph G.
+
+    The *augmented-Randic index* of a graph G with edge set *E* is defined as the
+    following sum
+
+    .. math::
+        \sum_{vw \in E} \frac{1}{\max(d_G(v), d_G(w))}
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        An undirected graph.
+
+    Returns
+    -------
+    float
+        The augmented Randic index of a graph.
+
+    References
+    ----------
+
+    Ivan Gutman, Degree-Based Topological Indices, Croat. Chem. Acta 86 (4)
+    (2013) 351–361. http://dx.doi.org/10.5562/cca2294
+    """
+    _degree = functools.partial(nx.degree, G)
+    return math.fsum((max(_degree(x), _degree(y)) ** -1 for x, y in G.edges()))
--- tests/test_topological_indices.py
+++ tests/test_topological_indices.py
@@ -11,3 +11,10 @@ class TestTopologicalIndices:
     def test_randic_index(self, graph, expected_value):
         """Ensure randic_index returns the expected value for a given graph"""
         assert gp.randic_index(graph) == expected_value
+
+    @pytest.mark.parametrize(
+        "graph, expected_value", ((gp.path_graph(2), 1), (gp.cycle_graph(3), 1.5), (gp.complete_graph(4), 2))
+    )
+    def test_augmented_randic_index(self, graph, expected_value):
+        """Ensure augmented_randic_index returns the expected value for a given graph"""
+        assert gp.augmented_randic_index(graph) == expected_value
