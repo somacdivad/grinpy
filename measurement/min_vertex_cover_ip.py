84632d3cfeca2e6e74a529a15c0a0bb0916885b4
David Amos <somacdivad@gmail.com>
2019-05-30 01:51:29 -0500

    Remove measurement directory

--- measurement/min_vertex_cover_ip.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('../')
-
-import grinpy as gp
-
-
-G = gp.circular_ladder_graph(10)
-vc = gp.min_vertex_cover_ip(G)
-print('Vertex Cover Number (ILP): {}'.format(len(vc)))
