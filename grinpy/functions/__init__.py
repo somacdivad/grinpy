a9cf78b46b189cbf980120a9aac137f0e6751423
David Amos <somacdivad@gmail.com>
2019-05-30 08:50:42 -0500

    Update imports

--- grinpy/functions/__init__.py
+++ grinpy/functions/__init__.py
@@ -1,5 +1,5 @@
-from .degree import *  # noqa
-from .distance import *  # noqa
-from .graph_operations import *  # noqa
-from .neighborhoods import *  # noqa
-from .structural_properties import *  # noqa
+from grinpy.functions.degree import *  # noqa
+from grinpy.functions.distance import *  # noqa
+from grinpy.functions.graph_operations import *  # noqa
+from grinpy.functions.neighborhoods import *  # noqa
+from grinpy.functions.structural_properties import *  # noqa
