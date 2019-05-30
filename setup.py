6541722faf14df9e037c52c1df898607a2f39343
David Amos <somacdivad@gmail.com>
2019-05-30 02:08:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,51 +3,69 @@
 
 """The setup script."""
 
+import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import sys
 
-with open('README.md') as readme_file:
+VERSION = "0.3.0"
+
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ['networkx==2.1',
-                'numpy==1.14.2',
-                'pulp==1.6.8']
+requirements = ["networkx>=2.1", "numpy>=1.14.2", "pulp>=1.6.8"]
 
-setup_requirements = ['pytest-runner',]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest',]
+test_requirements = ["pytest", "pytest-cov"]
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+
+    description = "verify that the git tag matches our version"
+
+    def run(self):
+        tag = os.getenv("CIRCLE_TAG")
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(tag, VERSION)
+            sys.exit(info)
+
 
 setup(
     author="David Amos",
-    author_email='somacdivad@gmail.com',
+    author_email="somacdivad@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Natural Language :: English",
     ],
     description="Graph invariants in Python.",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='grinpy',
-    name='grinpy',
-    packages=find_packages(include=['grinpy']),
+    keywords="grinpy",
+    name="grinpy",
+    packages=find_packages(include=["grinpy"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/somacdivad/grinpy',
-    version='0.2.0',
+    url="https://github.com/somacdivad/grinpy",
+    version=VERSION,
     zip_safe=False,
+    cmdclass={"verify": VerifyVersionCommand},
 )
