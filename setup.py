a9a0702666857d740678e867cd2b00cf52c76da4
David Amos <somacdivad@gmail.com>
2019-07-07 13:18:41 -0500

    Remove history from setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     description="Graph invariants in Python.",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="grinpy",
